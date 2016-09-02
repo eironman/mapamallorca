150b19c0b8912b7171929e34bdef44c4f6aa0b73
Aaron Amengual Arranz <aaron.amengual@gmail.com>
2016-09-02 08:32:46 +0200

    Prepared for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,71 @@
-const webpack = require('webpack');
+var path = require('path');
+var webpack = require('webpack');
+const environment = process.env.NODE_ENV || 'production';
 
-module.exports = {
-    entry    : './js/app/index.js',
-    output   : {
-        path    : './js/bundles',
-        filename: 'bundle.js'
+
+// Init configuration
+const config = {
+  output: {
+    path      : path.join(__dirname, 'js/build'),
+    filename  : 'bundle.js',
+    publicPath: '/js/build/'
+  },
+  
+  module: {
+    loaders: [
+    {
+      test   : /\.js$/,
+      exclude: /node_modules/,
+      loader : 'react-hot'
     },
-
-    devServer: {
-        colors: true,
-        historyApiFallback: true,
-        inline: true,
-        port: 9090
-    },
-
-    module: {
-        loaders: [
-        {
-            test   : /\.js$/,
-            exclude: /node_modules/,
-            loader : 'react-hot'
-        },
-        {
-            test   : /\.js$/,
-            exclude: /node_modules/,
-            loader : 'babel-loader',
-            query  : {
-                presets: ['es2015', 'react']
-            }
-        }]
-    },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
+    {
+      test   : /\.js$/,
+      exclude: /node_modules/,
+      loader : 'babel-loader',
+      query  : {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
 };
+
+// Entry point
+if (environment === 'local') {
+  
+  config.entry = [
+    'webpack-dev-server/client?http://localhost:9090',
+    'webpack/hot/only-dev-server',
+    './js/app/index.js'
+  ];
+
+} else {
+  config.entry = ['./js/app/index.js']
+}
+
+// Devtool (for debugging)
+if (environment === 'local') {
+  config.devtool = 'eval';
+}
+
+// Plugins
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify(environment)
+    }
+  })
+];
+if (environment === 'local') {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+} else {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }));
+}
+config.plugins = plugins;
+
+
+// Export config
+module.exports = config;
